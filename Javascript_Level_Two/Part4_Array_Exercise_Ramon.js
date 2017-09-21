917f523938054afd1712b498c0202b1f44321ecd
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-09-21 09:38:23 +0200

    Remove break, change remove to index

--- Javascript_Level_Two/Part4_Array_Exercise_Ramon.js
+++ Javascript_Level_Two/Part4_Array_Exercise_Ramon.js
@@ -38,7 +38,7 @@ function addNew(){
 //
 function remove(){
   var index = array.indexOf(prompt("Student to remove?"));
-  var removedItem = roster.splice(student, 1);
+  var removedItem = roster.splice(index, 1);
 }
 
 // Create a function called display that prints out the orster to the console.
@@ -50,8 +50,6 @@ function display(){
 var use = prompt("Do you want to use the roster app?(y/n)")
 if (use === "y"){
   tasks();
-} else {
-  break;
 }
 // Now create a while loop that keeps asking for an action (add,remove, display or quit)
 // Use if and else if statements to execute the correct function for each command.
