8124fbe82a090d400d28bcb2dc6a212d6428575c
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-09-29 09:25:57 +0200

    Comment all temporarily to ease testing of other parts of the projetct

--- jQuery/myfrontendproject.js
+++ jQuery/myfrontendproject.js
@@ -1,12 +1,15 @@
+// TEMP COMMENT OUT TO EASE TESTING OF OTHER PARTS
+
+
 // Get the names of the players
-var p1 = prompt('Name of player one?');
-var p2 = prompt('Name of player two?');
+// var p1 = prompt('Name of player one?');
+// var p2 = prompt('Name of player two?');
 
 // Toggle player names
-$('h2').text(function(event){
-  if ($(this).text() == ('Your play ' + p1)){
-    $(this).text() = ('Your play ' + p2);
-  } else {
-    $(this).text('Your play ' + p1);
-  }
-})
+// $('h2').text(function(event){
+//   if ($(this).text() == ('Your play ' + p1)){
+//     $(this).text() = ('Your play ' + p2);
+//   } else {
+//     $(this).text('Your play ' + p1);
+//   }
+// })
