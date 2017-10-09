a158cfda282661eb1ef86f43c9d33f6190ca91e0
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-10-09 13:51:49 +0200

    Finish exercise 1

--- Python_Level_One/Part9_Functions_Exercises.py
+++ Python_Level_One/Part9_Functions_Exercises.py
@@ -21,7 +21,17 @@
 # arrayCheck([1, 1, 2, 1, 2, 3]) → True
 
 def arrayCheck(nums):
-    # Check working file p9e1.py
+    ott = [1,2,3]
+    for ind, val in enumerate(nums):
+        if ind <= (len(nums)-3):
+            numSlice = [nums[ind], nums[ind+1],nums[ind+2]]
+            if numSlice == ott:
+                return True
+    return False
+print(arrayCheck([1, 1, 2, 3, 1]))
+print(arrayCheck([1, 1, 2, 4, 1]))
+print(arrayCheck([1, 1, 2, 1, 2, 3]))
+
 
 #####################
 ## -- PROBLEM 2 -- ##
--- Python_Level_One/p9e1.py
+++ Python_Level_One/p9e1.py
@@ -1,11 +1,11 @@
 def arrayCheck(nums):
-
     ott = [1,2,3]
-    for each in nums:
-        if ott in nums:
-            return True
-        else:
-            return False
+    for ind, val in enumerate(nums):
+        if ind <= (len(nums)-3):
+            numSlice = [nums[ind], nums[ind+1],nums[ind+2]]
+            if numSlice == ott:
+                return True
+    return False
 print(arrayCheck([1, 1, 2, 3, 1]))
 print(arrayCheck([1, 1, 2, 4, 1]))
 print(arrayCheck([1, 1, 2, 1, 2, 3]))
