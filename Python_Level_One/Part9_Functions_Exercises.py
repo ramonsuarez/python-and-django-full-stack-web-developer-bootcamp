ac62f5be644e2096b670aeee4c711ad3cffdcdd6
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-10-09 14:09:24 +0200

    Finish exercise 3

--- Python_Level_One/Part9_Functions_Exercises.py
+++ Python_Level_One/Part9_Functions_Exercises.py
@@ -72,7 +72,21 @@ print(stringBits('Heeololeo'))
 
 
 def end_other(a, b):
-  # CODE GOES HERE
+  aLow = str(a.lower())
+  bLow = str(b.lower())
+  lenA = len(a)
+  lenB = len(b)
+  if aLow[-3:] == bLow[-3:]:
+      return True
+  elif bLow[-3:] == aLow[-3:]:
+      return True
+  else:
+      return False
+
+
+print(end_other('Hiabc', 'abc'))
+print(end_other('AbC', 'HiaBc'))
+print(end_other('abc', 'abXabc'))
 
 #####################
 ## -- PROBLEM 4 -- ##
---
+++ Python_Level_One/p9e3.py
@@ -0,0 +1,19 @@
+
+
+
+def end_other(a, b):
+  aLow = str(a.lower())
+  bLow = str(b.lower())
+  lenA = len(a)
+  lenB = len(b)
+  if aLow[-3:] == bLow[-3:]:
+      return True
+  elif bLow[-3:] == aLow[-3:]:
+      return True
+  else:
+      return False
+
+
+print(end_other('Hiabc', 'abc'))
+print(end_other('AbC', 'HiaBc'))
+print(end_other('abc', 'abXabc'))
