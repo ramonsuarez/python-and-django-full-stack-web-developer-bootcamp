e56104e6ef8c2564b03c43902560ded9cb9c2fc9
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-10-16 13:25:27 +0200

    Solve leading 0 integer token error

--- Python_Level_One/p10game.py
+++ Python_Level_One/p10game.py
@@ -3,32 +3,34 @@ print('Welcome to Code Breaker\nGuess the right 3 digit number with the help of 
 import random
 digits = list(range(10))
 random.shuffle(digits)
-target = [str((digits[:3]))]
+nums = [str(i) for i in str(digits[:3]) if i.isdigit()]
+print(nums)
+target = []
+for num in nums:
+    target += num
+    print(target)
+# target = (lambda i: str(i) in nums, nums)
 print ('Target test: {}'.format(target))
 # DRY question function to get guess from user
+# Had to use raw_input because of Python 3 not accepting leading 0
 def question():
-    return list(str(input("What's your guess? ")))
+    print list(str(raw_input("What's your guess? ")))
 answer = question()
-# Test
-print(answer)
 # Ask for user's numbers until right solution
 gotIt = False
-while gotIt = False:
+while gotIt == False:
     #     Close: You've guessed a correct number but in the wrong position
     #     Match: You've guessed a correct number in the correct position
     #     Nope: You haven't guess any of the numbers correctly
-
-
-
-    # if answer == str(target):
-    #     print('Ole! You guessed the number right, it was {}'.format(answer))
-    #     gotIt = True
-    # elif (lambda x: answer(x) not in str(target), answer):
-    #     print('Here is the result of your guess:\n Nope')
-    #     answer = question()
-    # elif (lambda x: str(target)(x) == answer(x),answer):
-    #     print('Here is the result of your guess:\n Match')
-    #     answer = question()
-    # else:
-    #     print('Here is the result of your guess:\n Close')
-    #     answer = question()
+    if answer == target:
+        print('Ole! You guessed the number right, it was {}'.format(answer))
+        gotIt = True
+    elif (lambda x: target(x) == answer(x),answer):
+        print('Here is the result of your guess:\n Match')
+        answer = question()
+    elif (lambda x: answer(x) not in target, answer):
+        print('Here is the result of your guess:\n Nope')
+        answer = question()
+    else:
+        print('Here is the result of your guess:\n Close')
+        answer = question()
