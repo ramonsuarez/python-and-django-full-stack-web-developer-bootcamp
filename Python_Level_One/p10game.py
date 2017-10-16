6760bec5c3eb36f18724435df7187d8beb9557ab
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-10-16 10:29:21 +0200

    Loop ok

--- Python_Level_One/p10game.py
+++ Python_Level_One/p10game.py
@@ -3,14 +3,18 @@ print('Welcome to Code Breaker\nGuess the right 3 digit number with the help of 
 import random
 digits = list(range(10))
 random.shuffle(digits)
-print(digits[:3])
+target = (digits[:3])
+print ('Target test: {}'.format(target))
+
 # Ask for user's numbers until right solution
 gotIt = False
+answer = input("What's your guess? ")
 while gotIt != True:
-    answer = input("What's your guess?")
-    # Check answer and provide tips
-    if answer == digits[:3]:
-        print('Ole!')
-        break
-    if answer == 111:
-        break
+    answer_array = [int(i) for i in str(answer)]
+    if answer_array == target:
+        print('Ole! You guessed the number right, it was {}'.format(answer))
+        gotIt = True
+    elif (lambda x: x in set(target), answer_array ):
+        print('Here is the result of your guess:\n Match')
+        answer = input("What's your new guess? ")
+        gotIt = False
