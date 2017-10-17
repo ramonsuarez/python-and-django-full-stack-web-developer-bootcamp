1e92631466d25905fd5bf131926d4f2642493a88
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-10-17 09:56:29 +0200

    Solve issue with target index reference in match() function

--- Python_Level_One/p10game.py
+++ Python_Level_One/p10game.py
@@ -9,37 +9,38 @@ for num in nums:
     target += num
 # TEST
 print ('Target: {}'.format(target))
+print(type(target))
 # DRY question function to get guess from user
 # Had to use raw_input because of Python 3 not accepting leading 0
 def question():
     return list(str(raw_input("What's your guess? ")))
 # In case the number is longer than 3 digits, only take the first 3 digits
 answer = question()[:3]
+
 # Match function
 def match():
     for i, a in enumerate(answer):
-        print('a: {} target(i): {}'.format(a,target(i)))
-        # if answer(a) == (lambda t: t in target, target):
-        #     return True
-match()
+        print('a: {} i: {} target[i] {}'.format(a,i, target[i]))
+        if a == target[i]:
+            return True
 # # Close function
-# def close():
-#     for i in answer:
-#         if i in target:
-#             return True
+def close():
+    for i in answer:
+        if i in target:
+            return True
 # Ask for user's numbers, loop until right solution
-gotIt = False
-while gotIt == False:
-    # Correct answer
-    if answer == target:
-        print('Ole! You guessed the number right, it was {}'.format(answer))
-        gotIt = True
-    #     Match: You've guessed a correct number in the correct position
-
-    elif match() :
-        # elif (lambda x: target(x) == answer(x),answer):
-        print('Tip: Match')
-        answer = question()
+# gotIt = False
+# while gotIt == False:
+#     # Correct answer
+#     if answer == target:
+#         print('Ole! You guessed the number right, it was {}'.format(answer))
+#         gotIt = True
+#     #     Match: You've guessed a correct number in the correct position
+#
+#     elif match() :
+#         # elif (lambda x: target(x) == answer(x),answer):
+#         print('Tip: Match')
+#         answer = question()
 #         #     Nope: You haven't guess any of the numbers correctly
 #         # elif (lambda x: answer(x) not in target, answer):
 #     elif False in close():
