3069f709a5a66287395c5d8bf262c3eeba682dd0
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-11-08 12:08:12 +0100

    Create User model

--- exercises/django/ProTwo/AppTwo/models.py
+++ exercises/django/ProTwo/AppTwo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    firs_name: models.CharField(max_length=264)
+    last_name: models.CharField(max_length=264)
+    email: models.CharField(max_length=264, unique=True)
