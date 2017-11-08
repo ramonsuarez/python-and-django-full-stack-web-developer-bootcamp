d1f68b5d30167e3832ca6e76c51f6a4042cc0e33
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-11-08 12:00:33 +0100

    Table on index

--- exercises/django/FirstProject/first_app/views.py
+++ exercises/django/FirstProject/first_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from first_app.models import Topic, Webpage, Accessrecord
+from first_app.models import Topic, Webpage, AccessRecord
 
 def index(request):
     webpages_list = AccessRecord.objects.order_by('date')
