2ea18a73778bf37b58031cb74cdc22fc05380c1f
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-11-09 09:21:16 +0100

    New populate script

---
+++ exercises/django/ProTwo/populate_apptwo.py
@@ -0,0 +1,34 @@
+import os
+# Configure settings for project
+# Need to run this before calling models from application!
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','ProTwo.settings')
+
+import django
+# Import settings
+django.setup()
+
+import random
+from AppTwo.models import User
+from faker import Faker
+
+fakegen = Faker()
+
+def populate(N=5):
+    '''
+    Create N Entries of Dates Accessed
+    '''
+
+    for entry in range(N):
+
+        # Create Fake Data for entry
+        fake_first = fakegen.first_name()
+        fake_last = fakegen.last_name()
+        fake_email = fakegen.email()
+
+        # Create new User entry
+        user = User.objects.get_or_create(first_name=fake_first,last_name=fake_last, email=fake_email)[0]
+
+if __name__ == '__main__':
+    print("Populating the databases...Please Wait")
+    populate(20)
+    print('Populating Complete')
