f7d9b5a6b53636a4573b5934e67e0f58df5d6b21
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-11-09 11:20:14 +0100

    Create views

--- exercises/django/ProTwo/AppTwo/views.py
+++ exercises/django/ProTwo/AppTwo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from AppTwo.models import User
 
 def index(request):
+    # All users page
+    user_list = User.objects.order_by('first_name')
+    user_dict = {'user_list' : user_list}
+    return render(request,'users/index.html', context=user_dict)
+    # Help page
     my_dict = {'insert_me' : 'Hello this is views from AppTwo'}
     return render(request,'help/help.html', context=my_dict)
