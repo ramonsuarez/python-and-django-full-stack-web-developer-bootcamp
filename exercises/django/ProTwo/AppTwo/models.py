848dfcef80d28b8514374103334d6a308675c350
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-11-09 11:34:06 +0100

    Fix problem with db, start views

--- exercises/django/ProTwo/AppTwo/models.py
+++ exercises/django/ProTwo/AppTwo/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    firs_name = models.CharField(max_length=264)
+    first_name = models.CharField(max_length=264)
     last_name = models.CharField(max_length=264)
     email = models.CharField(max_length=264, unique=True)
