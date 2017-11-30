5e298bc3d595afe6fdc321a0bccc136fcb9ca6be
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-11-30 17:01:09 +0100

    Begin form

--- exercises/django/learning_users/basic_app/views.py
+++ exercises/django/learning_users/basic_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from basic_app.forms import UserForm, UserProfileInfoForm
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'basic_app/index.html')
+
+
+def register(request):
+    registered = False
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            # Hash password
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
