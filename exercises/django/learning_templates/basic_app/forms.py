8808543f94c2e23319089633d9afe1ade13caa9b
Ramon Suarez <ramon@serendipityaccelerator.com>
2017-12-11 14:04:36 +0100

    Mid registration lesson

--- exercises/django/learning_templates/basic_app/forms.py
+++ exercises/django/learning_templates/basic_app/forms.py
@@ -5,6 +5,11 @@ from basic_app.models import UserProfileInfo
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
-class Meta():
-    model = UserProfileInfoForm
-    fields = ('portfolio_site', 'profile_pic')
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileInfoForm(forms.ModelForm):
+    class Meta():
+        model = UserProfileInfo
+        fields = ('portfolio_site', 'profile_pic')
--- exercises/django/learning_templates/basic_app/views.py
+++ exercises/django/learning_templates/basic_app/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
-from basic_app.forms import UserFrom, UserProfileInfoForm
+from basic_app.forms import UserForm, UserProfileInfoForm
 
 def index(request):
     return render(request, 'basic_app/index.html')
-
-
-def other(request):
-    return render(request, 'basic_app/other.html')
-
 
 def register(request):
     registered = False
     if request.method == "POST":
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'profile.pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return: 
+        render(request, 'basic_app/registration.html')
