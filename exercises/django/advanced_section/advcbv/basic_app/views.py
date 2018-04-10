c6fdef58c525e66b8486ed2a7b4ff8dff3ceca3d
Ramon Suarez <ramon@serendipityaccelerator.com>
2018-04-10 12:38:11 +0200

    Finish part 1

--- exercises/django/advanced_section/advcbv/basic_app/views.py
+++ exercises/django/advanced_section/advcbv/basic_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
+from . import models
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -8,3 +10,13 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['injectme'] = 'Basic injection'
         return context
+
+
+class SchoolListView(ListView):
+    model = models.School
+
+
+class SchoolDetail(DetailView):
+    model = modes.School
+    template_name = 'basic_app/school_detail.html'
+
