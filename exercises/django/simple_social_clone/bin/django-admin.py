8a4d40b56f7e75674bae50579f94c68c139c9773
Ramon Suarez <ramon@serendipityaccelerator.com>
2018-05-08 09:41:19 +0200

    Move virtualenv from root ft folder to venv

--- exercises/django/simple_social_clone/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ramon/dev/djangofsu/exercises/django/simple_social_clone/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
