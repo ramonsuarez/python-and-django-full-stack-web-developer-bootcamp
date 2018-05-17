8818090291c61a6ee4ae7c3a2967e2e4080347fa
Ramon Suarez <ramon@serendipityaccelerator.com>
2018-05-17 16:47:59 +0200

    Finish Part 11

--- exercises/django/simple_social_clone/simplesocial_project/groups/urls.py
+++ exercises/django/simple_social_clone/simplesocial_project/groups/urls.py
@@ -1,4 +1,4 @@
-# Groups urls.def
+# Groups urls.py
 from django.conf.urls import url
 from . import views
 
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^$', views.ListGroups.as_view(), name='all'),
     url(r'^new/$', views.CreateGroup.as_view(), name = 'create'),
     url(r'posts/in/(?P<slug>[-\w]+)/$)', views.SingleGroup.as_view(), name = 'single'),
+    url(r'join/(?P<slug>[-\w]+)/$)', views.JoinGroup.as_view(), name = 'join'),
+    url(r'leave/(?P<slug>[-\w]+)/$)', views.LeaveGroup.as_view(), name = 'leave'),
 ]
